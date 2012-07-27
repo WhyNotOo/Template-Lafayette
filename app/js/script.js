55d310387c1ef1dcbd60a964d3346b3bef305a07
WhyNotOo <k.albessard@gmail.com>
2012-07-27 17:16:41 +0200

    Add scroller + custom background

--- app/js/script.js
+++ app/js/script.js
@@ -4,12 +4,14 @@
 
 (function ($) {
 
-var options = Joshfire.factory.config.template.options || {},
-  	brands  = Joshfire.factory.getDataSource("brands");
+var config = Joshfire.factory.config.app,
+		options = Joshfire.factory.config.template.options || {},
+  	brands  = Joshfire.factory.getDataSource("brands"),
+  	scroller;
 
 var app = {
 
-  displayBrands: function() {
+  searchBrands: function() {
   	if(brands) {
 	    brands.find({}, function (err, data) {
 	      if(err) {
@@ -17,26 +19,48 @@ var app = {
 	        alert('Une erreur est survenue pendant le chargement de l\'application. Merci de recharger la page.');
 	      } else {
 	        $.map(data.entries, function (entry, idx) {
-	          var brand = entry.entries;
-	          console.log(brand);
-
+	          var brand = entry;
+	          $('#brands ul').append('<li class="brand"><img src="'+brand['gsx:image']+'" alt="'+brand.name+'" /><h2>'+brand.name+'</h2></li>')
 	        });
+	        app.resizeItem();
 	      }
 	    });
+
+	    setTimeout(app.displayBrand, 1000);
 	  }
   },
 
-  addCustomConfig: function() {
-    if(Joshfire.factory.config.app.icon) {
-
-    }
-
-    if(Joshfire.factory.config.app.logo) {
-
-    }
-
-    app.displayBrands();
+  displayBrand: function() {
+  	$('.loader').addClass('hidden');
+  	$('#brands').removeClass('invisible');
+    scroller = new iScroll('brands', { hScroll: false, hideScrollbar: false, scrollbarClass: 'scrollbar' });
+    console.log(scroller);
   },
+
+  addCustomConfig: function() {
+    if(config.icon) { }
+
+    if(config.logo) {
+    	$('.app-header .logo').css('background-image', 'url('+Joshfire.factory.config.app.logo.contentURL+')');
+    }
+
+    if(options.backgroundimage) {
+    	$('#container').css('background-image', 'url('+options.backgroundimage+')');
+    }
+
+    app.searchBrands();
+  },
+
+	resizeItem: function() {
+		var brandW = $('.brand').width();
+		$('.brand').height(brandW+'px');
+  },
+
+  onResize: function() {
+		var brandW = $('.brand').width();
+		$('.brand').height(brandW+'px');
+		scroller.refresh();
+  }
 
 };
 // --- END APP --- \\
@@ -50,6 +74,7 @@ var app = {
 // --- LAUNCH APP --- \\
 $(document).ready(function() {
   app.addCustomConfig();
+  $(window).resize(app.onResize);
 });
 
 })(jQuery);
