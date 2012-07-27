d44db49e2faa51150715254023823bcb9a80064c
WhyNotOo <k.albessard@gmail.com>
2012-07-27 12:38:14 +0200

    Add package && marketing

--- app/js/script.js
+++ app/js/script.js
@@ -4,19 +4,52 @@
 
 (function ($) {
 
-  var app = {
+var options = Joshfire.factory.config.template.options || {},
+  	brands  = Joshfire.factory.getDataSource("brands");
 
-  	someName: function(param) {
+var app = {
 
-  	}
+  displayBrands: function() {
+  	if(brands) {
+	    brands.find({}, function (err, data) {
+	      if(err) {
+	        console.log('erreur : '+err);
+	        alert('Une erreur est survenue pendant le chargement de l\'application. Merci de recharger la page.');
+	      } else {
+	        $.map(data.entries, function (entry, idx) {
+	          var brand = entry.entries;
+	          console.log(brand);
 
-  };
-	// End app
+	        });
+	      }
+	    });
+	  }
+  },
+
+  addCustomConfig: function() {
+    if(Joshfire.factory.config.app.icon) {
+
+    }
+
+    if(Joshfire.factory.config.app.logo) {
+
+    }
+
+    app.displayBrands();
+  },
+
+};
+// --- END APP --- \\
 
 
-	// Launch app
-	$(document).ready(function() {
-	  //app.someName(param);
-	});
+
+// --- ACTIONS --- \\
+
+
+
+// --- LAUNCH APP --- \\
+$(document).ready(function() {
+  app.addCustomConfig();
+});
 
 })(jQuery);
